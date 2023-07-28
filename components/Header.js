684e0f05986f5c72692155cb011e495d2d94fde3
Rustin Dodd <rustin.dodd@ibm.com>
2023-07-28 13:00:41 -0500

    added menu and images

---
+++ components/Header.js
@@ -0,0 +1,39 @@
+// import library functionality
+import React, { useEffect, useState } from 'react';
+
+// import componenets
+
+import Icon from '@mui/material/Icon';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+
+export default function Header(props) {
+
+  const { setContent, showContent } = props;
+  const [selectedItem, setSelectedItem] = useState('code');
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    if (showContent) {
+      setVisible(true);
+    }
+  }, [showContent])
+
+  useEffect(() => {
+    setContent(selectedItem);
+  }, [selectedItem])
+
+  return (
+    <header>
+      <div className="logo">RD</div>
+      <div className={`menu ${selectedItem}${!visible ? ' hidden' : ''}`}>
+        <button onClick={() => setSelectedItem('code')}>
+          <Icon fontSize="large" component={TerminalIcon} />
+        </button>
+        <button onClick={() => setSelectedItem('music')}>
+          <Icon fontSize="large" component={LibraryMusicIcon} />
+        </button>
+      </div>
+    </header>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,49 @@
-import Head from 'next/head'
+// import library functionality
+import React, {useState, useEffect} from 'react';
+
+// import components
+import Head from 'next/head';
+import Header from '../components/Header';
+import Image from 'next/image';
 
 export default function Home() {
+
+  const [textOneVisible, setTextOneVisible] = useState(false);
+  const [textTwoVisible, setTextTwoVisible] = useState(false);
+  const [introTextSmall, setIntroTextSmall] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+  const [selectedContent, setSelectedContent] = useState('code');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTextOneVisible(true);
+    }, 500)
+    setTimeout(() => {
+      setTextTwoVisible(true);
+    }, 1500)
+    setTimeout(() => {
+      setIntroTextSmall(true);
+    }, 3000)
+    setTimeout(() => {
+      setShowContent(true);
+    }, 3500)
+  }, [])
+
   return (
-    <div className="app-container">
+    <>
       <Head>
         <title>Rustin Dodd - Austin, Texas based Front-End Developer nd Musician</title>
       </Head>
-      <div className="intro">
-        <h1>Well, hello there.</h1>
-      </div>
-    </div>
+      <Header showContent={showContent} setContent={setSelectedContent} />
+      <main>
+        <div className={`avatar${!showContent ? ' hidden' : ''}`}>
+          <Image src="/img/rustin-dodd.jpg" height="200" width="200" alt="Rustin Dodd" priority className={selectedContent != 'code' ? 'hidden' : ''} />
+          <Image src="/img/ezra-foster.jpg" height="200" width="200" alt="Ezra Foster" className={selectedContent != 'music' ? 'hidden' : ''} />
+        </div>
+        <div className={`intro${introTextSmall ? ' small' : ''}`}>
+          <h1><span className={!textOneVisible ? 'hidden' : '' }>Well, </span><span className={!textTwoVisible ? 'hidden' : '' }>hello there.</span></h1>
+        </div>
+      </main>
+    </>
   )
 }
