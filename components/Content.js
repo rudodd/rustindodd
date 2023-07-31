11f45bd8b9faa318b079483ca42135bdfb391b22
Rustin Dodd <rustin.dodd@ibm.com>
2023-07-31 14:19:22 -0500

    added music content and made responsive

--- components/Content.js
+++ components/Content.js
@@ -33,12 +33,12 @@ export default function Content(props) {
                   <p>Like most developers here in Austin, I moved here ~15 years ago to start my music career.  Luckily, I've been able to support my music habit by designing and building websites for damn near the whole time, though it didn't always pay so well.</p>
                   <p>I started my "real" career designing websites at a local IT Services company because, back in the day, people hired their IT company to build their website. I eventually grew tired of how other people coded my designs (designers can be picky) and learned how to code them myself.</p>
                   <p>Working at small companies means I wore a lot of hats.  I found myslef taking on marketing and minor management responsibilities in addition to my design and development roles, and that's what really changed everything.  I hated it.  I hated marketing, I got burned out on design, and realized I just wanted to code.</p>
-                  <p>Some time around 8 years ago, I decided to focus on becoming a full-time developer, only a developer, and a damn good developer.  Not sure that I'm a damn good developer yet, but I'm good.  I currently work for IBM on a team called Z DevOps Innovation Lab (I know, a mouthful) building internal applications using React and Next.js, and I love it.</p>
+                  <p>Some time around 8 years ago, I decided to focus on becoming a full-time developer, only a developer, and a damn good developer.  Not sure that I'm a damn good developer yet, but I'm good.  I currently work for IBM on a team called Z DevOps Innovation Lab (I know, a mouthful) building internal applications using React and Next.js.</p>
                 </>
               ) : storyLength == 'short' ? (
                 <>
                   <p>I started my career designing and building websites at a local IT Services company which was the first step in my journey into front-end development.  I spent nearly a decade working in small companies that required me to wear various hats that limited the time I spent doing development, and candidly, it took me too long to realize that I hated it.</p>
-                  <p>Some time around 8 years ago, I decided to focus soley on being a developer. I currently work for IBM on a team called Z DevOps Innovation Lab (I know, a mouthful) building internal applications using React and Next.js, and I love it.</p>
+                  <p>Some time around 8 years ago, I decided to focus soley on being a developer. I currently work for IBM on a team called Z DevOps Innovation Lab (I know, a mouthful) building internal applications using React and Next.js.</p>
                 </>
               ) : (
                 <p>I am a developer with 10+ years of experience.  I currently work for IBM building internal applications using React and Next.js.</p>
@@ -59,7 +59,31 @@ export default function Content(props) {
       ) : (
         <>
           <h3>I'm a song-writer and recording artist.</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dictum orci felis. Proin consequat dolor non lectus eleifend, et vulputate dui blandit. Cras non luctus sem. Morbi vel feugiat ante, imperdiet sagittis diam. Donec mattis ut justo quis pulvinar. Suspendisse ornare faucibus est ac rutrum. Mauris auctor quam in malesuada lacinia. Vivamus viverra leo ultricies massa auctor, at mattis est placerat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam scelerisque leo sit amet suscipit consequat. Phasellus ornare ante nisi, a venenatis erat placerat eu. Aliquam condimentum velit convallis finibus maximus.</p>
+          <p>Music is a through-line in my life.  I've had a love of music as long as I can remember and began to learn how to play instruments in the 5th grade.  I focus my energy on writing and recording.</p>
+          <h4>My projects:</h4>
+          <div className="music-projects">
+            <div>
+              <div className="artist-image">
+                <Image src="/img/artists/ezra-foster.jpg" width="200" height="150" alt="Ezra Foster" />
+              </div>
+              <h5>Ezra Foster</h5>
+              <Link href="https://www.ezrafoster.com" className="artist-button">Learn more</Link>
+            </div>
+            <div>
+              <div className="artist-image">
+                <Image src="/img/artists/dr-floydbeck.jpg" width="200" height="150" alt="Dr. Floydbeck" />
+              </div>
+              <h5>Dr. Floydbeck</h5>
+              <Link href="https://www.drfloydbeck.com" className="artist-button">Learn more</Link>
+            </div>
+            <div>
+              <div className="artist-image">
+                <Image src="/img/artists/crouch-and-foster.jpg" width="200" height="150" alt="Crouch and Foster" />
+              </div>
+              <h5>Crouch and Foster</h5>
+              <Link href="https://www.crouchandfoster.com" className="artist-button">Learn more</Link>
+            </div>
+          </div>
         </>
       )}
       
