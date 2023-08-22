5b2b8e4797ef104880114720bcc716e849113116
Rustin Dodd <rustin.dodd@ibm.com>
2023-08-22 10:41:40 -0500

    preloading artist images

--- components/Content.js
+++ components/Content.js
@@ -1,9 +1,9 @@
 // import library functionality
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import components
 import Icon from '@mui/material/Icon';
-import Image from 'next/image';
+import NextImage from 'next/image';
 import Link from 'next/link';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -13,12 +13,20 @@ export default function Content(props) {
   const { showContent, selectedContent } = props;
   const [storyLength, setStoryLength] = useState(null);
 
+  // Preload artist images on first render
+  useEffect(() => {
+    ['/img/artists/ezra-foster.jpg', '/img/artists/dr-floydbeck.jpg', '/img/artists/crouch-and-foster.jpg'].forEach((fileName) => {
+      const img = new Image();
+      img.src = fileName;
+    });
+  }, [])
+
   return (
     <div className={`content${!showContent ? ' hidden' : ''}`}>
       <h2>I'm Rustin Dodd.</h2>
       {selectedContent == 'code' ? (
         <>
-          <h3>I'm a front-end developer @ {<Link href="https://www.ibm.com"><Image src="/img/ibm.jpg" width="100" height="45" alt="IBM" priority /></Link>}.</h3>
+          <h3>I'm a front-end developer @ {<Link href="https://www.ibm.com"><NextImage src="/img/ibm.jpg" width="100" height="45" alt="IBM" priority /></Link>}.</h3>
           <p>I'm not sure what landed you here in my little corner of the internet, but I'm glad you came.  I assume you're here to learn more about me, so let's get to it...</p>
           <div className="length-selector">
             <p>Which would you prefer?</p>
@@ -66,21 +74,21 @@ export default function Content(props) {
           <div className="music-projects">
             <div>
               <div className="artist-image">
-                <Image src="/img/artists/ezra-foster.jpg" width="200" height="150" alt="Ezra Foster" priority />
+                <NextImage src="/img/artists/ezra-foster.jpg" width="200" height="150" alt="Ezra Foster" />
               </div>
               <h5>Ezra Foster</h5>
               <Link href="https://www.ezrafoster.com" className="artist-button">Learn more</Link>
             </div>
             <div>
               <div className="artist-image">
-                <Image src="/img/artists/dr-floydbeck.jpg" width="200" height="150" alt="Dr. Floydbeck" priority />
+                <NextImage src="/img/artists/dr-floydbeck.jpg" width="200" height="150" alt="Dr. Floydbeck" />
               </div>
               <h5>Dr. Floydbeck</h5>
               <Link href="https://www.drfloydbeck.com" className="artist-button">Learn more</Link>
             </div>
             <div>
               <div className="artist-image">
-                <Image src="/img/artists/crouch-and-foster.jpg" width="200" height="150" alt="Crouch and Foster" priority />
+                <NextImage src="/img/artists/crouch-and-foster.jpg" width="200" height="150" alt="Crouch and Foster" />
               </div>
               <h5>Crouch and Foster</h5>
               <Link href="https://www.crouchandfoster.com" className="artist-button">Learn more</Link>
